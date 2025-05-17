fc1c6b790e59f7c87dc57c62b69990494f2cc112
Fouad Magdy <fouad.magdy772@gmail.com>
2025-05-17 23:00:58 +0300

    Initial Commit

---
+++ Connect Sphere/Connect_SphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  Connect_SphereApp.swift
+//  Connect Sphere
+//
+//  Created by Fouad Magdy on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Connect_SphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connect SphereTests/Connect_SphereTests.swift
@@ -0,0 +1,17 @@
+//
+//  Connect_SphereTests.swift
+//  Connect SphereTests
+//
+//  Created by Fouad Magdy on 17/05/2025.
+//
+
+import Testing
+@testable import Connect_Sphere
+
+struct Connect_SphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
